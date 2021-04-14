289bef6207fbb30bb30946af1aaf4f906a821828
Raiymbek <raymbek@MacBook-Pro-Raymbek.local>
2021-04-14 23:07:52 +0600

    ...

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TitlePageComponent } from './title-page/title-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: '/:id', component: TitlePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
